ab3379cba02f4935d4a6c6f8246cb31f81ae0125
Kyle Brown <kyle_brown@intuit.com>
2016-04-07 08:07:25 -0700

    fix defaults, add unit tests

--- test/index.js
+++ test/index.js
@@ -23,4 +23,103 @@ describe('constructor', () => {
     });
 
     // TODO: finish tests
-})
+});
+
+describe('retry', () => {
+
+    it('should delete retry config from options', (done) => {
+        const request = (options, cb) => {
+
+            expect(options.retry).to.not.exist();
+            cb();
+        };
+        const options = {
+            retry: {
+                test: true
+            }
+        };
+        const gofer = new GoferRetry();
+        gofer.retry('test', request, options, (err) => {
+            expect(err).to.not.exist();
+            done();
+        });
+    });
+
+    it('should set attempts metadata', (done) => {
+        const request = (options, cb) => {
+            cb(null, {}, {});
+        };
+        const options = {};
+        const gofer = new GoferRetry();
+        gofer.retry('test', request, options, (err, body, meta) => {
+            expect(err).to.not.exist();
+            expect(meta).to.be.an.object();
+            expect(meta.attempts).to.equal(1);
+            done();
+        });
+    });
+
+    it('should set errors metadata', (done) => {
+        const request = (options, cb) => {
+            cb(new Error('test-error'), {}, {});
+        };
+        const options = {
+            retry: {
+                retries: 0
+            }
+        };
+        const gofer = new GoferRetry();
+        gofer.retry('test', request, options, (err, body, meta) => {
+            expect(err).to.exist();
+            expect(meta).to.be.an.object();
+            expect(meta.attempts).to.equal(1);
+            expect(meta.errors).to.be.an.array();
+            expect(meta.errors).to.have.length(1);
+            expect(meta.errors[0]).to.deep.equal(err);
+            done();
+        });
+    });
+
+    it('should use endpoint defaults', (done) => {
+        const request = (options, cb) => {
+            cb(new Error('test-error'));
+        };
+        const options = {};
+        const gofer = new GoferRetry();
+        gofer.endpointDefaults = {
+            test: {
+                retry: {
+                    retries: 1
+                }
+            }
+        }
+        gofer.retry('test', request, options, (err, body, meta) => {
+            expect(err).to.exist();
+            expect(meta).to.be.an.object();
+            expect(meta.errors).to.have.length(2);
+            expect(meta.attempts).to.equal(2);
+            done();
+        });
+    });
+});
+
+describe('_retryMapper', () => {
+    it('should set retry config retries property to 0 if not defined and retry config exists', (done) => {
+        const options = {
+            retry: {}
+        };
+        const result = GoferRetry._retryMapper(options);
+        expect(result).to.be.an.object();
+        expect(result.retry).to.be.an.object();
+        expect(result.retry.retries).to.equal(0);
+        done();
+    });
+    it('should not set retry config if it doesn\'t already exist', (done) => {
+        const options = {};
+        const result = GoferRetry._retryMapper(options);
+        expect(result).to.be.an.object();
+        expect(result.retry).to.not.exist();
+        expect(result).to.deep.equal(options);
+        done();
+    });
+});
